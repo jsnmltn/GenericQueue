4497755a645ec914c85c47c67109e50b086cf6de
jsnmltn <112011162+jsnmltn@users.noreply.github.com>
2023-03-15 04:02:25 -0400

    -Implementing various methods to add new features

--- src/QTester.java
+++ src/QTester.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class QTester {
     public static void main(String[] args) {
 
@@ -27,7 +28,22 @@ public class QTester {
         System.out.println("Number of items in the Queue: " + q.size());
         System.out.println("Next element in the que has a value of: " + q.peek());
 
-
+        List<String> q2 = Arrays.asList("David", "John", "Mary", "Susan", "Kim", "George", "Alan", "Lisa", "Micheal");
+        q2.stream().map(str->str.toUpperCase());
+        System.out.println("The new Array List of names contains the following names:");
+        //print out each element in the array list on a seperate line
+        for(String name : q2) {
+            System.out.println(name.toUpperCase());
+        }
+        System.out.println("Now we will use the sort method to organize the names in alphabetical order: ");
+        q2.sort(Comparator.naturalOrder());
+        for(String name : q2) {
+            System.out.println(name.toUpperCase());
+        }
         sc.close();
+        
+        //q2.iteratorforEach(System.out.println(q2);
+
+        
     }
 }
